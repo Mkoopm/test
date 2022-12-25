d726ec0c4c1b59432bfa0c618e7336e6be31ffa7
Marten Koopmans <marten.koopmans@ordina.nl>
2022-12-25 13:42:07 +0100

    Add Monte Carlo area calculateor

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import numpy as np
 
 
 class Shape(ABC):
@@ -7,12 +8,45 @@ class Shape(ABC):
         pass
 
 
+class AreaCalculator(ABC):
+    shape: Shape
+
+    @abstractmethod
+    def calc_area() -> float:
+        pass
+
+
 class Square(Shape):
-    def is_inside(x: float, y: float) -> bool:
+    def __init__(self) -> None:
+        pass
+
+    def is_inside(self, x: float, y: float) -> bool:
         inside = True
-        if x < 0 or x > 1:
+        if x < 0 or x > 0.5:
             inside = False
-        if y < 0 or y > 1:
+        if y < 0 or y > 0.5:
             inside = False
 
         return inside
+
+
+class MonteCarloAreaCalculator:
+    def __init__(self, shape: Shape) -> None:
+        self.shape = shape
+
+    def calc_area(self) -> float:
+        n_points = 10000
+        x_arr = np.random.rand(n_points)
+        y_arr = np.random.rand(n_points)
+        print(f"length = {len(x_arr)}")
+        n_inside = 0
+        n_outside = 0
+        for x, y in zip(x_arr, y_arr):
+            if self.shape.is_inside(x, y):
+                n_inside += 1
+            else:
+                n_outside += 1
+
+        area = n_inside / n_points
+        print(f"area = {area}")
+        return area
--- test_main.py
+++ test_main.py
@@ -2,6 +2,20 @@ import main
 
 
 def test_square():
-    square = main.Square
+    square = main.Square()
 
     assert square.is_inside(2, 1) == False
+
+
+def test_monte_carlo_area_calculator():
+    square = main.Square()
+
+    MCA_calculator = main.MonteCarloAreaCalculator(square)
+    allowed_error = 0.01
+    true_area = 0.25
+    calced_area = MCA_calculator.calc_area()
+    print(f"calculated area = {calced_area}, true area = {true_area}")
+    assert (
+        calced_area < true_area + allowed_error
+        and calced_area > true_area - allowed_error
+    )
